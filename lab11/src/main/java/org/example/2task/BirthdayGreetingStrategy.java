d1129a63d579a023d09d5e95425e5b0cbf6aaa30
MaxDzioban <dzoban.pn@ucu.edu.ua>
2024-11-25 23:20:05 +0200

    upload

---
+++ lab11/src/main/java/org/example/2task/BirthdayGreetingStrategy.java
@@ -0,0 +1,9 @@
+package org.example.stask;
+
+class BirthdayGreetingStrategy implements MailContentStrategy {
+    @Override
+    public String generateContent(Client client) {
+        return "Happy Birthday " + client.getName() + "! "
+                + "Special gift for you: Not a talon!";
+    }
+}
